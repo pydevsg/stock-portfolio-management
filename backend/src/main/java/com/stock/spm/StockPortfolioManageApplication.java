3e9109dc80eb4dc45fe21ef5cd15193eb164e04b
pydevsg <ghoshsudipto1129@gmail.com>
2025-03-31 03:14:50 +0100

    backend-core-setup

---
+++ backend/src/main/java/com/stock/spm/StockPortfolioManageApplication.java
@@ -0,0 +1,12 @@
+package com.stock.spm;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockPortfolioManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockPortfolioManageApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stock/spm/StockPortfolioManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.spm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockPortfolioManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
